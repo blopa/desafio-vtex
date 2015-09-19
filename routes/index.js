50bc5cfb22354a9a1ebb9363cd071d5d23e037d9
Pablo Montenegro <montenegro.pablo@outlook.com>
2015-09-19 19:30:25 -0300

    adicionando api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var GitHubApi = require("github");
+
+var github = new GitHubApi({
+  // required
+  version: "3.0.0",
+  // optional
+  debug: true,
+  protocol: "https",
+  host: "api.github.com", // should be api.github.com for GitHub
+  timeout: 5000,
+  headers: {
+    "user-agent": "GitHub-VTEX-API-Test" // GitHub is happy with a unique user agent
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  github.orgs.getMembers({
+    org: "AmazingWorks"
+  }, function(err, data) {
+
+    function obter_dados_usuario(user){
+      github.user.repobertt
+
+    }
+    data.forEach(obter_dados_usuario);
+
+    res.render('index', { title: 'VTEX', 'git':data });
+
+  });
+
 });
 
 module.exports = router;
