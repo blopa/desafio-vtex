a21b210b29092f5761544ea87dbf5c6d187a6cef
Pablo Montenegro <montenegro.pablo@outlook.com>
2015-09-19 22:54:48 -0300

    mudanças

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,65 @@
 var express = require('express');
 var router = express.Router();
-var GitHubApi = require("github");
+var request = require('request');
+var when = require('when');
 
-var github = new GitHubApi({
-  // required
-  version: "3.0.0",
-  // optional
-  debug: true,
-  protocol: "https",
-  host: "api.github.com", // should be api.github.com for GitHub
-  timeout: 5000,
-  headers: {
-    "user-agent": "GitHub-VTEX-API-Test" // GitHub is happy with a unique user agent
-  }
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  github.orgs.getMembers({
-    org: "AmazingWorks"
-  }, function(err, data) {
-
-    function obter_dados_usuario(user){
-      github.user.repobertt
-
+  // todos os membros de uma organização
+  request.get({
+    url: 'https://api.github.com/orgs/AmazingWorks/members',
+    headers: {
+      'User-Agent': 'Vtex-blopa'
     }
-    data.forEach(obter_dados_usuario);
+  }, function(error, response, body){
+    var usuarios_repositorio = JSON.parse(body);
+    var usuarios = [];
+    var promises = [];
 
-    res.render('index', { title: 'VTEX', 'git':data });
+    usuarios_repositorio.forEach(function obter_dados_usuario(user){
+      var sinalizador_usuario = when.defer();
+
+      // obter todos os dados de um membro da organizacao (followers)
+      request.get({
+        url: 'https://api.github.com/users/' + user.login,
+        headers: {
+          'User-Agent': 'Vtex-blopa'
+        }
+      }, function(error, response, body){
+
+        var usuario = JSON.parse(body);
+        usuario.qtd_star = 0;
+        usuarios.push(usuario);
+
+        // obter a qtd de stars dos repositorios de um usuario
+        request.get({
+          url: 'https://api.github.com/users/' + user.login + '/repos',
+          headers: {
+            'User-Agent': 'Vtex-blopa'
+          }
+        }, function(error, response, body){
+          var premisses_repositorios = [];
+
+          var repositorios = JSON.parse(body);
+          repositorios.forEach(function(repositorio) {
+            usuario.qtd_star += repositorio.stargazers_count;
+          });
+          // depois de obter todos os commits de cada um dos repositorios
+          sinalizador_usuario.resolve();
+        });
+
+      });
+      promises.push(sinalizador_usuario.promise);
+
+    });
+
+    when.all(promises).then(function () {
+
+      res.render('index', { title: 'VTEX', usuarios:usuarios });
+
+    });
 
   });
 
